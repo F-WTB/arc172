fba97307964b0100c49ada79cbc2d1116be375c3
F <63239460+F-WTB@users.noreply.github.com>
2024-03-03 04:12:24 +0900

    [update]a solved

--- a.cpp
+++ a.cpp
@@ -1,18 +1,54 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-
-const int N = 6;
-int A[N];
-
-bool b = 0;
-
+using namespace atcoder;
+using ll = long long;
+using pr = pair<int, int>;
+using mint = modint998244353;
+ostream &operator<<(ostream &os, const mint &x)
+{
+    os << x.val();
+    return os;
+}
+struct fast
+{
+    fast()
+    {
+        cin.tie(0);
+        ios::sync_with_stdio(0);
+    }
+} fast;
+int H, W, N, A;
+ll c[31], d[31];
 int main()
 {
-    for (int i = N - 1; i >= 0; --i)
+    cin >> H >> W >> N;
+    for (int i = 31 - 1; i >= 0; --i)
+        for (int j = 31 - 1; j >= 0; --j)
+        {
+            if (H & 1 << i && W & 1 << j)
+            {
+                int k = min(i, j);
+                c[k] += ll(1 << i - k) * (1 << j - k);
+            }
+        }
+
+    for (int i = 0; i < N; ++i)
     {
-        cout << i << '\n';
-        if (b)
-            return 0;
-        A[i - 1] = 0;
+        cin >> A;
+        ++d[A];
     }
+
+    for (int i = 31 - 1; i >= 0; --i)
+    {
+        if (d[i] > c[i])
+        {
+            cout << "No" << '\n';
+            return 0;
+        }
+        c[i] -= d[i];
+        if (i)
+            c[i - 1] += c[i] * 4;
+    }
+    cout << "Yes\n";
 }
